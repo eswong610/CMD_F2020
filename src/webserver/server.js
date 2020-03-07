4b9ecf3c404ccde49a921bc56d0bca7d6b156d90
Emmy <ewong96@live.ca>
2020-03-07 15:05:17 -0800

    file rework

--- src/webserver/server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const { port, entry } = require('./globals')
-const server = express()
-
-server.listen( port, () => console.log( `\nServer live at http://localhost:${port}` ) )
