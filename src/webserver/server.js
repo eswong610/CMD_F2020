b49f2fd4afd6051e869883aa3b8b2f7064f054fd
jennormous <smj.boudreau@gmail.com>
2020-03-07 14:01:23 -0800

    connecting mongoDB database to server

--- src/webserver/server.js
+++ src/webserver/server.js
@@ -1,5 +1,19 @@
-const express = require('express')
-const { port, entry } = require('./globals')
-const server = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const { port, entry } = require("./globals");
+const server = express();
 
-server.listen( port, () => console.log( `\nServer live at http://localhost:${port}` ) )
+//connect to mongo
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("connected to DB!"))
+  .catch((err) => console.log(err));
+
+server.listen(port, () =>
+  console.log(`\nServer live at http://localhost:${port}`)
+);
